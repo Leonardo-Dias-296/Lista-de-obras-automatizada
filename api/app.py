582be2afcffb7bcc6a6e64325f2bf1927d3be976
Leonardo Dias <leonardodiasrosa14@gmail.com>
2026-06-28 21:27:50 -0300

    debug: test flask_cors import

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/config')
 def config():
-    return jsonify({"test": "ok"})
+    return jsonify({"test": "ok with CORS"})
